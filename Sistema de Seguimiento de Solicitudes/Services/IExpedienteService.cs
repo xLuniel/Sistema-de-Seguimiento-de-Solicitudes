5eedac604fee9e17fe57d41bb9ad2efd34495664
xLuniel <l20211827@tectijuana.edu.mx>
2025-03-19 14:52:25 -0700

    Cambios

--- Sistema de Seguimiento de Solicitudes/Services/IExpedienteService.cs
+++ Sistema de Seguimiento de Solicitudes/Services/IExpedienteService.cs
@@ -1,17 +1,14 @@
 ﻿using SolicitudesShared;
-using Sistema_de_Seguimiento_de_Solicitudes.Services;
 
 namespace Sistema_de_Seguimiento_de_Solicitudes.Services
 {
     public interface IExpedienteService
     {
-        Task<List<ExpedienteDTO>> Lista();
-
-        Task<ExpedienteDTO> Obtener(int id);
-
         Task<ResponseAPI<ExpedienteDTO>> Actualizar(ExpedienteDTO expediente);
-        Task<int> Crear(ExpedienteDTO Expediente);
+        Task<List<ExpedienteDTO>> Lista();
+        Task<ExpedienteDTO> Obtener(int id);
         Task<int> Editar(ExpedienteDTO Expediente);
+        Task<int> Crear(ExpedienteDTO NuevoExpediente);
         Task<bool> Eliminar(int id);
     }
 }
