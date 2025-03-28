bd6fdd81a08bb824afd0c4196014a3ffed264898
Yael Quintana <l20212116@tectijuana.edu.mx>
2025-03-27 18:23:42 -0700

    Movido los Catalogos  a Catalogos.cs en la carpeta models para mantener un codigo mas limpio

---
+++ Sistema de Seguimiento de Solicitudes/Models/Catalogos.cs
@@ -0,0 +1,26 @@
+﻿namespace Sistema_de_Seguimiento_de_Solicitudes.Models
+{
+    public class Catalogos
+    {
+        public static readonly List<string> MesesAdmision = new()
+            {
+                "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+                "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+            };
+
+        public static readonly List<string> Estatuses = new()
+            {
+                "En proceso", "Pendiente", "Terminado","Cancelado"
+            };
+
+        public static readonly List<string> Ciudades = new()
+            {
+                "Tijuana", "Mexicali", "Ensenada", "Rosarito", "Tecate"
+            };
+
+        public static readonly List<string> Cobros = new()
+            {
+                "Ninguno", "Si"
+            };
+    }
+}
