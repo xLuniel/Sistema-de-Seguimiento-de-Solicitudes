cfc0e6ff277b3b04b48d03c5613829ccd480f3d9
xLuniel <l20211827@tectijuana.edu.mx>
2025-04-16 17:01:26 -0700

    Modificacion de Calendario (Falta)
    Se agrega paginacion

---
+++ Sistema de Seguimiento de Solicitudes/wwwroot/js/pagsize.js
@@ -0,0 +1,23 @@
+﻿window.getPageSizeBasedOnResolution = () => {
+    const width = window.innerWidth;
+
+    // Define tus rangos de resolución
+    if (width < 576) {
+        // Por ejemplo, dispositivos móviles pequeños
+        return 8;
+
+    }
+    if (width < 1370) {
+        // Móviles en horizontal o pantallas algo mayores
+        return 10;
+
+    }
+    if (width < 2050) {
+        // Tablet
+        return 12;
+
+    } else {
+        // Escritorio y pantallas grandes
+        return 15;
+    }
+};
