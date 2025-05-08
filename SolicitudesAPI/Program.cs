d0d562bd2a1e2691e29eed07778b328ef24a2507
Yael Quintana <l20212116@tectijuana.edu.mx>
2025-05-07 22:59:25 -0700

    navmenu navlinkmatch

--- SolicitudesAPI/Program.cs
+++ SolicitudesAPI/Program.cs
@@ -48,7 +48,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ValidateIssuerSigningKey = true,
             ValidIssuer = builder.Configuration["Jwt:Issuer"],
             ValidAudience = builder.Configuration["Jwt:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"]))
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"]!))
         };
     });
 
