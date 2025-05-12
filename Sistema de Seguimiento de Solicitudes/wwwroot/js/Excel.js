06ac62845c567ac3cd3e05e770e2d2c4279b0fbd
xLuniel <l20211827@tectijuana.edu.mx>
2025-05-12 16:17:56 -0700

    Script para descargar a Excel

---
+++ Sistema de Seguimiento de Solicitudes/wwwroot/js/Excel.js
@@ -0,0 +1,12 @@
+﻿window.excelFunctions = {
+    descargarExcel: function (nombreArchivo, base64Archivo) {
+        console.log("Iniciando descarga de Excel...");
+        const link = document.createElement("a");
+        link.download = nombreArchivo;
+        link.href = "data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64," + base64Archivo;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        console.log("Descarga completada");
+    }
+};
