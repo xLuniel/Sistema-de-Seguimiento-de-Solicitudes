be781fde65f1ee20cf1050c839eb5494d02ab47e
xLuniel <l20211827@tectijuana.edu.mx>
2025-05-19 17:47:38 -0700

    Ver todas las solicitudes en calendario

--- Sistema de Seguimiento de Solicitudes/Models/EventoSolicitud.cs
+++ Sistema de Seguimiento de Solicitudes/Models/EventoSolicitud.cs
@@ -5,5 +5,8 @@
         public string Title { get; set; } = "";
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
+        public string TipoSolicitud { get; set; } = "";
+        public bool TienePrevencion { get; set; }
+        public bool EsUrgente { get; set; }
     }
 }
